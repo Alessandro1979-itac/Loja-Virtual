ccdb2472ba4e315cbe2039ce26c52023649a6d46
Alessandro <sandromuniz27@hotmail.com>
2022-08-02 16:18:48 -0400

    initial commit Primeiros passos no Projeto

---
+++ LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LojaVirtual.Models;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            Produto produto = GetProduto();
+            return View(produto);
+            //return new ContentResult() { Content = "<h3>Produto -> Vizualizar</h3>", ContentType = "text/html" };
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox One X",
+                Descricao = "Jogue em 4k",
+                Valor = 2000.00M
+            };
+        }
+    }
+}
